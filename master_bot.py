63ae4270ac4e1b070b3b1cc7c115482165df5387
Leo Oliveira <macewandu@hotmail.com>
2018-08-09 23:00:20 -0400

    ?

--- master_bot.py
+++ master_bot.py
@@ -67,7 +67,6 @@ logger.addHandler(handler)
 # Header to make the requests
 headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
 
-
 # ------------------------------------------------------------- #
 #                                                               #
 #                 Method to make use of TinyURL                 #
