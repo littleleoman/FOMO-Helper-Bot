8c023e1bba8a9766e1646b4629292a440b8e0794
FOMO <info@fomo.supply>
2019-03-04 14:07:14 -0300

    Update master_bot.py

--- master_bot.py
+++ master_bot.py
@@ -958,9 +958,15 @@ async def shopifyTools(ctx):
 async def shopify_check(ctx, url):
     check = shopify.shopify_check(url)
     if check == True:
-        await client.send_message(ctx.message.channel, ":white_check_mark: [THE URL]({}) __**IS**__ A SHOPIFY WEBSITE!".format(url))
+        embed = discord>embed(title=":white_check_mark: [***THE URL***]({}) __***IS***__ ***A SHOPIFY WEBSITE!***", color=0xffffff)
+        embed.set_footer(icon_url=icon_img, text=footer_text)
+        await client.send_message(ctx.message.author, embed=embed)
+        #await client.send_message(ctx.message.channel, ":white_check_mark: [THE URL]({}) __**IS**__ A SHOPIFY WEBSITE!".format(url))
     elif check == False:
-        await client.send_message(ctx.message.channel, ":no_entry_sign: [THE URL]({}) __**IS NOT**__ A SHOPIFY WEBSITE!".format(url))
+        embed = discord>embed(title=":no_entry_sign: [***THE URL***]({}) __***IS NOT***__ ***A SHOPIFY WEBSITE!***", color=0xffffff)
+        embed.set_footer(icon_url=icon_img, text=footer_text)
+        await client.send_message(ctx.message.author, embed=embed)
+        #await client.send_message(ctx.message.channel, ":no_entry_sign: [THE URL]({}) __**IS NOT**__ A SHOPIFY WEBSITE!".format(url))
 ### END SHOPIFY TOOLS ---------------------------------------------------------------------------------------------- END SHOPIFY TOOLS ###
 
 ### START SOLEBOX ------------------------------------------------------------------------------------------------------------ START SOLEBOX
