b62b5e0aad6e8479a80a4f4367f09c462e8f994e
FOMO <info@fomo.supply>
2019-03-04 05:51:51 -0300

    Update master_bot.py

--- master_bot.py
+++ master_bot.py
@@ -157,7 +157,8 @@ async def on_message(message):
                 SUCCESS_POSTER.success_poster(author, image)
 ### MONITOR PINGER ----------------------------------------------------------------------------------------- MONITOR PINGER ###
     embeds = message.embeds
-    keywords = k
+    keywords = requests.get('https://jsonblob.com/api/jsonBlob/1a574529-3e55-11e9-bd88-f34ccbbcde5d')
+    keywords = json.loads(k.text)
     if len(embeds) == 0:
         for keyword in keywords:
             sub_keys = keywords[keyword]
